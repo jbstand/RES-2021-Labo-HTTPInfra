f36c0b89090151cd9c20f159dab5cdf74b6cdf83
Jeremie Melly <melly.jeremie@gmail.com>
2021-06-04 12:50:29 +0200

    Script to update backend IPs from env variables

--- dyn_reverse/setup.sh
+++ dyn_reverse/setup.sh
@@ -2,8 +2,24 @@
 
 # Add setup for RES lab
 echo "Setup for RES lab..."
-echo "Static app URL : $STATIC_APP"
-echo "Dynamic app URL : $DYNAMIC_APP"
+echo "Static backend URLs : $STATIC_BACKEND"
+echo "Dynamic backend URLs : $DYNAMIC_BACKEND"
+
+# Creation of the backend and insertion in the nginx conf file
+static_backend=""
+
+for i in $STATIC_BACKEND
+do 
+static_backend="$static_backend\t\tserver $i;\n"
+done
+
+dynamic_backend=""
+
+for i in $DYNAMIC_BACKEND
+do 
+dynamic_backend="$dynamic_backend\t\tserver $i;\n"
+done
+
 sed -i "s/STATIC_URL/$STATIC_APP/" /etc/nginx/conf.d/default.conf
 sed -i "s/DYNAMIC_URL/$DYNAMIC_APP/" /etc/nginx/conf.d/default.conf
 
